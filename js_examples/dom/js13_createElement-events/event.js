5d82a76da7866e0561b8a8569ca278a8691a0db4
BruceHill <brucehillwalley@gmail.com>
2023-11-02 22:51:12 +0300

    events

--- js_examples/dom/js13_createElement-events/event.js
+++ js_examples/dom/js13_createElement-events/event.js
@@ -1,6 +1,9 @@
 const header=document.querySelector("header")
 const addButton=document.getElementById("btn")
+const input=document.querySelector("#input")
+const ul=document.querySelector("ul")
 
+//? 3.yöntem
 header.onmouseover =function(){
     header.style.backgroundColor="red"
     header.style.color="white"
@@ -12,6 +15,29 @@ header.onmouseout =function(){
 }
 
 
+const yazdir=()=>{
+    document.write("Hello from Turkey")
+}
+
+
+//?onload event ı html ve css kodlarının render edilmesinden sonra çalışır
+// window.onload=yazdir()
+    
+
+//?window yüklendiğinde input a fokuslan
+window.addEventListener("load",()=>{
+    input.focus()
+})
+
+
+//? 4.yöntem addeventlistener metodu
+//*add butonuna tıklandığında event fonksiyonu çağırılır
 addButton.addEventListener("click",()=>{
-    alert("tıklandı")
+    const li =document.createElement("li")
+    const text=document.createTextNode(input.value)
+    li.appendChild(text)
+    ul.appendChild(li)
+    input.value=""
+    input.focus()
+
 })
