263eec51bed022547f03ae1af5dd22f8dc9a7eea
BruceHill <brucehillwalley@gmail.com>
2023-11-04 00:05:03 +0300

    event

--- js_examples/dom/js13_createElement-events/event.js
+++ js_examples/dom/js13_createElement-events/event.js
@@ -1,43 +1,59 @@
-const header=document.querySelector("header")
-const addButton=document.getElementById("btn")
-const input=document.querySelector("#input")
-const ul=document.querySelector("ul")
+const header = document.querySelector("header");
+const addButton = document.getElementById("btn");
+const input = document.querySelector("#input");
+const ul = document.querySelector("ul");
 
 //? 3.yöntem
-header.onmouseover =function(){
-    header.style.backgroundColor="red"
-    header.style.color="white"
-}
+header.onmouseover = function () {
+  header.style.backgroundColor = "red";
+  header.style.color = "white";
+};
 
-header.onmouseout =function(){
-    header.style.backgroundColor="transparent"
-    header.style.color="black"
-}
+header.onmouseout = function () {
+  header.style.backgroundColor = "transparent";
+  header.style.color = "black";
+};
 
-
-const yazdir=()=>{
-    document.write("Hello from Turkey")
-}
-
+const yazdir = () => {
+  document.write("Hello from Turkey");
+};
 
 //?onload event ı html ve css kodlarının render edilmesinden sonra çalışır
 // window.onload=yazdir()
-    
 
 //?window yüklendiğinde input a fokuslan
-window.addEventListener("load",()=>{
-    input.focus()
-})
-
+window.addEventListener("load", () => {
+  input.focus();
+});
 
 //? 4.yöntem addeventlistener metodu
 //*add butonuna tıklandığında event fonksiyonu çağırılır
-addButton.addEventListener("click",()=>{
-    const li =document.createElement("li")
-    const text=document.createTextNode(input.value)
-    li.appendChild(text)
-    ul.appendChild(li)
-    input.value=""
-    input.focus()
+addButton.addEventListener("click", () => {
+  if (!input.value.trim()) {
+    
+    alert("lütfen bilgi giriniz");
+  } else {
+    const li = document.createElement("li");
+    const text = document.createTextNode(input.value);
+    li.appendChild(text);
+    ul.appendChild(li);
+    input.value = "";
+    input.focus();
+  }
+});
 
+//? input elenmenti üzeerine basılıp 
+input.addEventListener("keyup",(event)=>{
+    // console.log(event);
+    // console.log(event.target);
+    // console.log(event.keyCode);
+    // console.log(event.code);
+
+
+//?
+    if (event.code==='Enter') {
+        addButton.click()
+    }
 })
+
+//!EKLEDİKLERİMİZİ BUTONLA SONDAN SİLME VE OLMAYANI EKLEME
