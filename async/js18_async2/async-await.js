9147c4dfd231f9babef72bead8204f0bd0ee0e45
BruceHill <brucehillwalley@gmail.com>
2023-11-17 17:44:10 +0300

    news

--- async/js18_async2/async-await.js
+++ async/js18_async2/async-await.js
@@ -19,48 +19,46 @@
 //* Yapilan istek yerine getirilip sonuc degerlerinin dondurulmesi ile kodun calismasi devam eder.
 
 const getNews = async () => {
-  const API_KEY = "1a1a999e0d7240a6bd2dead87bcca78err"
-  const BASE_URL = "https://newsapi.org/v2/"
-  const queryString = "top-headlines?country=us&category=sport&"
+  const API_KEY = "c5f41f8f198d48d6b05fc398c6a3ac49";
+  const BASE_URL = "https://newsapi.org/v2/";
+  const queryString = "top-headlines?country=us&category=sports&";
 
-  // const res = await fetch(`https://newsapi.org/v2/top-headlines?country=tr&category=sport&apiKey=1a1a999e0d7240a6bd2dead87bcca78e`)
+  // const res= await fetch(`https://newsapi.org/v2/top-headlines?country=tr&category=sports&apiKey=c5f41f8f198d48d6b05fc398c6a3ac49`)
 
   try {
-    const res = await fetch(`${BASE_URL}${queryString}apiKey=${API_KEY}`)
+    const res = await fetch(`${BASE_URL}${queryString}apiKey=${API_KEY}`);
     //? Error handling
     if (!res.ok) {
-      throw new Error(`Something went wrong:${res.status}`)
+      throw new Error(`Something went wrong:${res.status}`);
     }
-    const data = await res.json()
-    renderNews(data.articles)
-  } catch (error) {
-    const newsDiv = document.getElementById("news-div")
-    newsDiv.innerHTML = `
-        <h2>${error}</h2>
-        <img src="./img/404.png" alt="" />
-    `
-  }
-}
-const renderNews = (news) => {
-  const newsDiv = document.getElementById("news-div")
+    const data = await res.json();
 
-  news.forEach((item) => {
-    const { title, urlToImage, url, content } = item
-    newsDiv.innerHTML += `
-    <div class="col-sm-6 col-md-4 col-lg-3">
-        <div class="card">
-            <img src="${urlToImage}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${title}</h5>
-                <p class="card-text">${content}</p>
-                <a href="${url}" target="_blank" class="btn btn-primary">Detail</a>
-            </div>
-        </div>
-    </div>
-    `
-  })
-}
+    renderNews(data.articles);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const renderNews = (news) => {
+const newsDiv=document.getElementById("news-div")
+news.forEach((element) => {
+ 
+  const {title,urlToImage,url,content}=element
+  newsDiv.innerHTML+=`
+<div class="col-sm-6 col-md-4 col-lg-3">
+  <div class="card" >
+  <img src="${urlToImage}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${title}</h5>
+    <p class="card-text">${content}</p>
+    <a href="${url}" target="_blank" class="btn btn-primary">Detail</a>
+  </div>
+  </div>
+</div>`
+});
+
+};
 
 window.addEventListener("load", () => {
-  getNews()
-})
+  getNews();
+});
