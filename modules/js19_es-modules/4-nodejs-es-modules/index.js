89298155473c809a0dcf7e47321cf2d0b18080e3
BruceHill <brucehillwalley@gmail.com>
2023-11-20 22:50:02 +0300

    vite

--- modules/js19_es-modules/4-nodejs-es-modules/index.js
+++ modules/js19_es-modules/4-nodejs-es-modules/index.js
@@ -1,4 +1,9 @@
 import { topla } from "./hesap.js";
+import * as emoji from 'node-emoji'
+// import axios from 'axios'
 
 console.log(topla(3,5));
 
+console.log(emoji.random()
+);
+//? terminalde npm start la görüntüle
