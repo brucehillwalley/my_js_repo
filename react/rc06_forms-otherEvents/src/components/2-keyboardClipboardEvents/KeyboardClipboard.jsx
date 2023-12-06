2c553b232d6dc980fd990e38416151b6d888b123
BruceHill <brucehillwalley@gmail.com>
2023-12-06 23:47:14 +0300

    ok

--- react/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ react/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,8 +1,59 @@
+import { useState } from "react";
 
 const KeyboardClipboard = () => {
-  return (
-    <div>KeyboardClipboard</div>
-  )
-}
+  const [icereik, setIcereik] = useState("");
 
-export default KeyboardClipboard
+  const handleKeyDown = (e) => {
+    console.log(e.keyCode);
+    if (e.keyCode >= 47 && e.keyCode <= 58) {
+      alert("Girdiginiz deger rakam olamaz");
+      e.preventDefault();
+    }
+  };
+
+  const handleCopy = (e) => {
+    alert("kopyalandi");
+  }
+
+  const handleCut = (e) => {
+    alert("kesme işlemi yapıldı");
+    e.preventDefault()
+  }
+
+  const handlePaste = (e) => {
+    e.target.style.backgroundColor = "red"
+    e.target.style.fontSize = "30px"
+  }
+
+  return (
+    <div className="container text-center d-flex flex-column align-items-center mt-4"  >
+      <h2>Keyboard Event</h2>
+      <input
+        type="text"
+        className="form-control"
+        onChange={(e) => setIcereik(e.target.value.toLocaleUpperCase())}
+        value={icereik}
+        onKeyDown={handleKeyDown}
+      />
+      <div className="w-50 mt-3">
+        <p onCopy={handleCopy} className="border border-dark">{icereik}</p>{" "}
+        <p onCopy={handleCopy} className="border border-dark">{icereik.toLocaleLowerCase()}</p>{" "}
+        <p>deneme</p>
+      </div>
+
+      <div>
+
+        <textarea onCut={handleCut} onCopy={handleCopy} onPaste={handlePaste}
+          className="form-control"
+          name="textarea"
+          id="area"
+          cols="30"
+          rows="10"
+        ></textarea>
+
+      </div>
+    </div>
+  );
+};
+
+export default KeyboardClipboard;
