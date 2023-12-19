6f35edb3ffdd3b9d11b85e80b021cd646915db81
BruceHill <brucehillwalley@gmail.com>
2023-12-19 18:20:26 +0300

    spinner

--- react/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ react/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import NotFound from "./NotFound";
+import spinner from "../img/spinner.gif"
 
 const PersonDetail = () => {
   const { idx } = useParams();
   const navigate = useNavigate();
   const [person, setPerson] = useState([]);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
+ 
   const getPerson = () => {
     fetch(`https://reqres.in/api/users/${idx}`)
       .then((res) => {
         if (!res.ok) {
+          setLoading(false)
           setError(true);
           throw new Error("Something went wrong");
         }
         return res.json();
       })
-      .then((data) => setPerson(data.data))
+      .then((data) => {
+        setLoading(false)
+        setPerson(data.data)})
       .catch((err) => console.log(err));
   };
 
@@ -25,11 +31,14 @@ const PersonDetail = () => {
     getPerson();
   }, []);
 
-  console.log(person);
-
-  if (error) {
+  console.log(loading);
+  
+  if(loading){
+return <div className="container text-center mt-4"><img src={spinner} alt="" /></div> 
+  }else if (error) {
     return <NotFound />;
-  } else {
+  } 
+  else {
     return (
       <div className="container text-center mt-4">
         <img className="rounded" src={person?.avatar} alt="img" />
