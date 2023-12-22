b9a14c0e99cffd7fab95e97a9fd71ab5243a31b7
BruceHill <brucehillwalley@gmail.com>
2023-12-23 00:07:57 +0300

    tailwind component card

--- react/rc13_tailwind/src/App.js
+++ react/rc13_tailwind/src/App.js
@@ -1,18 +1,22 @@
 import FlexAndResponsive from "./components/FlexAndResponsive";
 import HoverFocus from "./components/HoverFocus";
+import Navbar from "./components/Navbar";
 import SpacingAndSizing from "./components/SpacingAndSizing";
 import Typografy from "./components/Typografy";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-2xl text-center text-purple-700 font-bold">
+      <Navbar />
+      {/* <h1 className="text-2xl text-center text-purple-700 font-bold">
         Hello Tailwind
-      </h1>
+      </h1> */}
       {/* <Typografy /> */}
       {/* <SpacingAndSizing /> */}
       {/* <HoverFocus /> */}
-      <FlexAndResponsive />
+      {/* <FlexAndResponsive /> */}
+      <Profile />
     </div>
   );
 }
