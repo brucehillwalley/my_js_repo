aeb18e9d4565ad13b9eb971e7cce354c3bfd459c
BruceHill <brucehillwalley@gmail.com>
2024-01-04 21:01:18 +0300

    authSlice

---
+++ react/redux/redux-toolkit-inClass/src/features/authSlice.jsx
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: { email: "", password: "" },
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, {type, payload}) => {
+        state.user=payload
+
+    },
+    clearUser: (state) => {
+        state.user={ email: "", password: "" }
+    },
+  },
+});
+
+export const {setUser,clearUser}=authSlice.actions
+
+export default authSlice.reducer
