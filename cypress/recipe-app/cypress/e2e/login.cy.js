a4a13859a0e197498ff0c615c8cc3280fa435f3c
BruceHill <brucehillwalley@gmail.com>
2024-02-04 14:47:21 +0300

    login test

--- cypress/recipe-app/cypress/e2e/login.cy.js
+++ cypress/recipe-app/cypress/e2e/login.cy.js
@@ -1,6 +1,17 @@
-describe('login', () => {
-  it('login test', () => {
-    cy.visit('http://localhost:3000')
-  })
-})
+describe("login", () => {
+  it("login test", () => {
+    cy.visit("http://localhost:3000");
 
+    cy.get('[data-test="loginHeader"]').contains("Recipe")
+    cy.get('[data-test="loginName"]').should('be.visible').type("asp")
+    //! should ile bir iddaa dabulunuuyoruz örn. görünüyor mu? type ile içine yazıyoruz
+    cy.get('[data-test="loginPassword"]').should('be.visible').type("Clarusway7")
+    cy.get('[data-test="loginSbmt"]').should("be.visible").click({force:true})
+    //! click ile butona tıklatıyoruz.  force true ile her türlü çalıştırıyoruz.
+    cy.url().should('include', '/home')
+    //! urlde home sayfasına gitti mi kontrol ediyoruz
+
+
+
+  });
+});
