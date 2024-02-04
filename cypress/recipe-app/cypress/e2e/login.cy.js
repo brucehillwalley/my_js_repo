f8d91e032077b6ae12ac5eab8d693c62ca9b479b
BruceHill <brucehillwalley@gmail.com>
2024-02-04 15:07:50 +0300

    login finished

--- cypress/recipe-app/cypress/e2e/login.cy.js
+++ cypress/recipe-app/cypress/e2e/login.cy.js
@@ -1,17 +1,30 @@
 describe("login", () => {
+
+beforeEach(() => {
+  cy.visit("http://localhost:3000/");
+  cy.viewport(1200, 1200)
+  //! viewport ile ekranı ayarlıyoruz
+})
+
   it("login test", () => {
-    cy.visit("http://localhost:3000");
+  
 
-    cy.get('[data-test="loginHeader"]').contains("Recipe")
-    cy.get('[data-test="loginName"]').should('be.visible').type("asp")
-    //! should ile bir iddaa dabulunuuyoruz örn. görünüyor mu? type ile içine yazıyoruz
-    cy.get('[data-test="loginPassword"]').should('be.visible').type("Clarusway7")
-    cy.get('[data-test="loginSbmt"]').should("be.visible").click({force:true})
+    cy.get('[data-test="loginHeader"]').contains("Recipe");
+    cy.get('[data-test="loginName"]').should("be.visible").type("asp");
+    //! should ile bir iddaada bulunuyoruz örn. görünüyor mu? type ile içine yazıyoruz
+    cy.get('[data-test="loginPassword"]')
+      .should("be.visible")
+      .type("Clarusway7");
+    //! type ile inputa yazıyoruz
+    //? cy.wait(20000)
+    //! 20 saniye bekletioruz istersek
+    cy.get('[data-test="loginSbmt"]')
+      .should("be.visible")
+      .click({ force: true });
     //! click ile butona tıklatıyoruz.  force true ile her türlü çalıştırıyoruz.
-    cy.url().should('include', '/home')
+    cy.url().should("include", "/home");
     //! urlde home sayfasına gitti mi kontrol ediyoruz
-
-
-
   });
 });
+
+
