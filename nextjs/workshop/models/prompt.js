fa0810f8f0de93b2014b5dc873537f63111c8d23
BruceHill <brucehillwalley@gmail.com>
2024-02-06 22:17:53 +0300

    prompt model

---
+++ nextjs/workshop/models/prompt.js
@@ -0,0 +1,25 @@
+import { Schema, model, models } from "mongoose";
+
+const promptSchema = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: String,
+    required: [true, "Prompt is required!"],
+  },
+  header: {
+    type: String,
+    required: [true, "Header is required!"],
+  },
+  tags: {
+    type: [String],
+    required: [true, "Tags are required!"],
+  },
+});
+
+const Prompt = models.Prompt || model("Prompt", promptSchema);
+//? prompt modeliş varsa al yoksa oluştur
+
+export default Prompt;
