c84fb3fdafff6ab4a2752eeab86787aab744f546
BruceHill <brucehillwalley@gmail.com>
2024-02-20 15:08:20 +0300

    tw works

--- nodejs/04-NodeJS_Setup/index.js
+++ nodejs/04-NodeJS_Setup/index.js
@@ -4,3 +4,38 @@ console.log("ibrahim");
 console.log(`------*------
     ${node}
 ------*------`);
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+console.log((0.1 + 0.2).toFixed(2) === "0.30"); // Output: true
+console.log(`------*------`);
+console.log(1 < 2 < 3);
+console.log(true < 3);
+console.log(3 > 2 > 1);
+console.log(true > 1);
+console.log(`------*------`);
+
+// let sum = 0;
+// const prompt = require("prompt-sync")();
+// let number = parseInt(prompt("Enter a number: "));
+
+// while (number >= 0) {
+//   // Add all positive numbers
+//   sum += number;
+
+//   console.log(`The sum is ${sum}.`);
+//   // Take input again if the number is positive
+//   number = parseInt(prompt("Enter a number: "));
+// }
+
+// const prompt = require("prompt-sync")({sigint:true})
+// const name = prompt('What is your name? ');
+// console.log("Hello " + name);
+
+//? 4.
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(isNaN(2 + null));
+console.log(isNaN(2 + undefined));
+null ? console.log("true") : console.log("false") 
+
+console.log(`------*------`);
